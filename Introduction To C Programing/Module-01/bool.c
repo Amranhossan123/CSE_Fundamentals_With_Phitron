43ceed4c58e229ff8528c39f8214615d1c21c33a
Amranhossan123 <mdimranh080@gmail.com>
2026-04-05 23:17:34 +0600

    Variables and Data Types

--- Introduction To C Programing/Module-01/bool.c
+++ Introduction To C Programing/Module-01/bool.c
@@ -2,7 +2,7 @@
 #include<stdbool.h>
 int main(){
     bool b=true;
-    printf("%d\n",b);
+    printf("%d\n",b); //boolean er jony alada kono formate specifier nei vai re.
      b=false;
     printf("%d",b);
     return 0;

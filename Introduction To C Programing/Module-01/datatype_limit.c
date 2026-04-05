a4e764a0c05c8168f6a6c39e15528013112dcb0a
Amranhossan123 <mdimranh080@gmail.com>
2026-04-05 23:24:53 +0600

    Data Types and Variables

--- Introduction To C Programing/Module-01/datatype_limit.c
+++ Introduction To C Programing/Module-01/datatype_limit.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main(){
-    int a=1000000000000;
+    int a=1000000000000; //int 10^9
     printf("%d\n",a);
-    long long int b=1000000000;
+    long long int b=1000000000; //long long int 10^18
     printf("%lld\n",a);
-    float f=45667.567899987;
+    float f=45667.567899987; //total 8 digit
     printf("%f\n",f);
     double d=45667.567899;
     printf("%lf",f);

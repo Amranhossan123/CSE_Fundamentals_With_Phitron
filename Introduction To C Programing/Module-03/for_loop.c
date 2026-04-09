5c4642c1525462c51e7023e786feb8a381b1c711
Amranhossan123 <mdimranh080@gmail.com>
2026-04-09 12:32:01 +0600

    Loop

--- Introduction To C Programing/Module-03/for_loop.c
+++ Introduction To C Programing/Module-03/for_loop.c
@@ -10,11 +10,20 @@
 // }
 
 
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+//     for(int i=n;i>=1;i--){
+//         printf("%d\n",i);
+//     }
+//     return 0;
+// }
+
 #include<stdio.h>
 int main(){
-    int n;
-    scanf("%d",&n);
-    for(int i=n;i>=1;i--){
+    int n=200;
+    for(int i=8;i<=200;i+=8){
         printf("%d\n",i);
     }
     return 0;
---
+++ Introduction To C Programing/Module-03/while_loop.c
@@ -0,0 +1,24 @@
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+//     int i=1;
+//     while (i<n)
+//     {
+//         printf("%d\n",i);
+//         i++;
+//     }
+//     return 0;
+// }
+
+#include<stdio.h>
+int main(){
+    int i=0,j=1;
+    while(j<=5){
+        i++;
+        j++;
+    }
+    printf("%d",j);
+
+    return 0;
+}

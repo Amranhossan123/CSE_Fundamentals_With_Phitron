9b2e3ac897a9d40ac906d34c53c4b08ff05698e1
Amranhossan123 <mdimranh080@gmail.com>
2026-04-10 21:59:02 +0600

    Assignment-01

---
+++ Introduction To C Programing/Module-04/problem-5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    if(1500<=n){
+        printf("I will buy Punjabi\n");
+        printf("I will buy new shoes\n");
+        printf("Alisa will buy new shoes\n");
+    }else if (1000<n && 1500>n)
+    {
+        printf("I will buy Punjabi\n");
+    }else{
+        printf("Bad luck!\n");
+    }
+    
+    return 0;
+}
